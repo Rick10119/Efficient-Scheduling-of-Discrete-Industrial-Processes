1ad137bd67aff7076237213eb275ccc78ed8c9dd
Ruike Lyu (吕睿可) <47710448+Rick10119@users.noreply.github.com>
2025-01-18 17:52:03 +0800

    更新发布版代码

--- rtn_model/test_rtn_gap.m
+++ rtn_model/test_rtn_gap.m
@@ -3,16 +3,8 @@ clc;
 clear;
 
 NOF_HEAT = 8;
-% for gap = [1, 1e-1, 1e-2, 1e-3]
-%     for day_index = [5:8, 11:13, 15, 19:22, 26:28]
-%         disp("day_index: " + day_index);
-%         disp("gap: " + gap);
-%         main_flxb_rtn;
-%     end
-% end
-
-for gap = [1e-3, 1e-4]
-    for day_index = [5:8, 11:13, 15]
+for gap = [1, 1e-1, 1e-2, 1e-3]
+    for day_index = [5:8, 11:13, 15, 19:22, 26:28]
         disp("day_index: " + day_index);
         disp("gap: " + gap);
         main_flxb_rtn;
--- rtn_model/test_rtn_heat.m
+++ rtn_model/test_rtn_heat.m
@@ -2,21 +2,13 @@
 clc;
 clear;
 
-for NOF_HEAT = [1, 2, 3, 4, 5, 6, 7]
+for NOF_HEAT = [1, 2, 3, 4, 5, 6, 7, 8]
     for day_index = [5:8, 11:13, 15, 19:22, 26:28]
         disp("day_index: " + day_index);
         disp("NOF_HEAT: " + NOF_HEAT);
         main_flxb_rtn;
     end
 end
-
-% for NOF_HEAT = [7, 8, 9, 10]
-%     for day_index = [26]
-%         disp("day_index: " + day_index);
-%         disp("NOF_HEAT: " + NOF_HEAT);
-%         main_flxb_rtn;
-%     end
-% end
 
 
 
